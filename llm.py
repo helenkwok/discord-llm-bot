476a38a3c7a0fab3d4dc8d6f1cd077745ee2528f
Helen Kwok <107752876+helenkwok@users.noreply.github.com>
2023-08-14 22:11:56 +0930

    Add discord bot and langchain function

---
+++ llm.py
@@ -0,0 +1,22 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+)
+
+# setup an instance of the OpenAI language model
+chat = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.5)
+
+template = "You are an assistant that helps users find information."
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template = "{question}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages(
+  [system_message_prompt, human_message_prompt])
+
+
+def getResponse(prompt):
+  response = chat(chat_prompt.format_prompt(question=prompt).to_messages())
+  return response
---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+import discord
+
+from llm import getResponse
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  call = '$llm '
+  if message.content.startswith(call):
+    response = getResponse(message.content.split(call)[1])
+    await message.channel.send(response.content)
+
+
+client.run(DISCORD_TOKEN)
